d2e6a44b9fdf9ba26e065497f9c646a844580b46
ThakareSakshi <sakshithakare121@gmail.com>
2024-03-28 20:17:52 +0530

    uploaded ecomm backend

---
+++ utils/sendmail.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+const dotenv=require("dotenv")
+dotenv.config()
+
+const sendEmailhandler = async (email, subject, text) => {
+	try {
+		console.log("inside mail sender")
+		const transporter = nodemailer.createTransport({
+			host: "smtp.gmail.com",
+			service: 'gmail',
+			port: 587,
+			secure: true,
+			
+			auth: {
+				user: "sakshithakare121@gmail.com",
+				pass: "lzaa qpmj qagd kxf",
+			},
+			authentication: 'plain',
+             enable_starttls_auto: true,
+
+		});
+		console.log("sending mail",process.env.EMAIL_KEY);
+
+
+		await transporter.sendMail({
+			from: "sakshithakare121@gmail.com",
+			to: email,
+			subject: subject,
+			text: text,
+		});
+		console.log("email sent successfully");
+	} catch (error) {
+		console.log("email not sent!");
+		console.log(error);
+		return error;
+	}
+};
+
+module.exports = {sendEmailhandler}
